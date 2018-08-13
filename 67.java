603cd4d7202a8241067ebf16f58c469e1068e786
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-13 12:52:28 +0530

    Create 67.java

---
+++ 67.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		n=(n/10)*10;
+		n += 10;
+		System.out.println(n);
+	}
+}
