f3461ad672a5dad01a1666972f3bc01af2c7083f
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-13 13:38:25 +0530

    Create 70.java

---
+++ 70.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ide
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int n,m,res;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int a=n*2;
+		
+		System.out.println(a);
+	}
+}
