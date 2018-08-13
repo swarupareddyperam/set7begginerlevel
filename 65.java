60ce5d06bc8b5fe9ee344b9e9c4949b10c7346d3
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-13 12:01:08 +0530

    Create 65.java

---
+++ 65.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Null
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  String ch;
+		Scanner sc=new Scanner(System.in);
+		ch=sc.nextLine();
+		if(System.out.printf(ch)==null)
+		{
+		sc.close();	
+		}
+	}
+}
