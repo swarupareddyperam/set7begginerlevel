07bfa7862456e8cc9d8dd7f30d473ccd0207d3cc
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-13 12:55:43 +0530

    Create 68.java

---
+++ 68.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%7==0)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
